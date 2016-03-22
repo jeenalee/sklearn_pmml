f182ddabaabb40eabc55baf9298f9ed369d24980
Jeena Lee <ijeenalee@gmail.com>
2016-03-22 09:31:09 -0700

    Add randomforest to __init__.py

--- sklearn_pmml/extensions/__init__.py
+++ sklearn_pmml/extensions/__init__.py
@@ -1,5 +1,6 @@
 import sklearn_pmml.extensions.linear_regression
-
+#import sklearn_pmml.extensions.logistic_regression
+import sklearn_pmml.extensions.random_forest
 # TODO:
 # import .extensions.svm
 # import .extensions.random_forest
